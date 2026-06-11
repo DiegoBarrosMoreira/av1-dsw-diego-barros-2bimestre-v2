3f101d12724849d22c036724a0988e6af60a2230
EA - Thais Barros Moreira <thais_barros@educadventista.org>
2026-06-11 07:54:07 -0300

    feat: implement CORS middleware using cors package

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@
 // - Registrar as rotas
 // - Preparar a aplicação para ser exportada
 
+import cors from "cors";
 import express from "express";
 import tarefaRoutes from "./routes/tarefaRoutes.js";
 
@@ -24,15 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Habilita CORS para permitir chamadas do frontend Vite local
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(cors());
+
 
 // ========================================
 // ROTAS
